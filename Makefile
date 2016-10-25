737a75b8eeebaf5584e29c8cdc5288f5030972f1
Josh Cox <josh@webhosting.coop>
2016-10-25 09:06:22 -0500

    poseidon

--- Makefile
+++ Makefile
@@ -10,3 +10,14 @@ server: serverplay
 serverplay:
 	ansible-playbook kvmsible.yaml
 
+auto: IP NAME
+
+IP:
+	@while [ -z "$$IP" ]; do \
+		read -r -p "Enter the IP you wish to associate with this KVM host [IP]: " IP; echo "$$IP">>IP; cat IP; \
+	done ;
+
+NAME:
+	@while [ -z "$$NAME" ]; do \
+		read -r -p "Enter the name you wish to associate with this  [NAME]: " NAME; echo "$$NAME">>NAME; cat NAME; \
+	done ;
--- packer.sh
+++ packer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo hello
+TMP=$(mktemp -d --suffix=PACKERTMP))
+export PACKER_VERSION=0.11.0
+export PACKER_URL=https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip
+export PACKER_ZIP=packer_0.11.0_linux_amd64.zip
 #wget -c https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip >/dev/null 2>&1; echo 0
-cd /tmp
-wget -c https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip 
+cd $TMP
+wget -c $PACKER_URL
 cd /usr/local/bin
-unzip -o /tmp/packer_0.8.6_linux_amd64.zip
+unzip -o "$TMP/$PACKER_ZIP"
+rm -Rf $TMP
