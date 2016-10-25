8e64b4bd1b05f524973ab667de1a298b170c67c2
Josh Cox <josh@webhosting.coop>
2016-10-25 09:31:48 -0500

    bootstrap

---
+++ bootstrapansible.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+apt-get update
+apt-get install -y python python-pip
+pip install docker-py
--- packer.sh
+++ packer.sh
@@ -2,8 +2,10 @@
 
 TMP=$(mktemp -d --suffix=PACKERTMP)
 export PACKER_VERSION=0.11.0
-export PACKER_URL=https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip
-export PACKER_ZIP=packer_0.11.0_linux_amd64.zip
+export PACKER_URL="https://releases.hashicorp.com/packer/$PACKER_VERSION/packer_$(echo -n $PACKER_VERSION)_linux_amd64.zip"
+export PACKER_ZIP="packer_$(echo -n $PACKER_VERSION)_linux_amd64.zip"
+echo $PACKER_URL
+echo $PACKER_ZIP
 
 # Download and unpack Packer
 cd $TMP
